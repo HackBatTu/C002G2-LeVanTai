b75122ef094eb0eb58d09ed21f5c23159ef8dc92
C0222G2_LeVanTai <levantai1496@gmail.com>
2022-04-02 08:26:29 +0700

    nop_bai_ss1

--- module2/src/ss1_introduction/bai_tap/NumberToString.java
+++ module2/src/ss1_introduction/bai_tap/NumberToString.java
@@ -6,34 +6,62 @@ public class NumberToString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the num");
-        int num = sc.nextInt();
+        String number = sc.nextLine();
 
-        String str;
-        switch (num){
+    }
+    private static String tenSlowTen(String number) {
+        switch (number) {
             case "0":
-                 return " ";
+                return "";
             case "1":
                 return "one";
-            case 2:
-                str = "two";
-            case 3:
-                str = "three";
-            case 4:
-                str = "four";
-            case 5:
-                str = "five";
-            case 6:
-                str = "six";
-            case 7:
-                str = "seven";
-            case 8:
-                str = "eight";
-            case 9:
-                str = "night";
+            case "2":
+                return "two";
+            case "3":
+                return "three";
+            case "4":
+                return "four";
+            case "5":
+                return "five";
+            case "6":
+                return "six";
+            case "7":
+                return "seven";
+            case "8":
+                return "eight";
+            case "9":
+                return "nine";
             default:
-                str = "out of ability";
+                return "out of ability";
+        }
+    }
+    private static String tenNextTen(String number, String nextTen) {
+        switch (number) {
+            case "0":
+                return nextTen;
+            case "1":
+                switch (nextTen) {
+                    case "":
+                        return "ten";
+                    case "one":
+                        return "eleven";
+                    case "two":
+                        return "twelve";
+                    case "three":
+                        return "thirdteen";
+                    default:
+                        return nextTen + "teen";
+                }
+            case "2":
+                return "twenty " + nextTen;
+            case "3":
+                return "thirdty " + nextTen;
+            default:
+                return tenSlowTen(number) + "ty" + " " + nextTen;
 
         }
-        System.out.print(" dãy số là : " + str);
+    }
+    private static String hunred(String number) {
+        return tenSlowTen(number) + " hunred";
     }
 }
