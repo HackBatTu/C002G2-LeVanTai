09cf020ae1c06414a26b318e20207a88748fae11
C0222G2_LeVanTai <levantai1496@gmail.com>
2022-05-13 10:01:16 +0700

    nop_bai_ss1

---
+++ module3/ss1_co_so_du_lieu/bai_tap/QuanLiStudent.sql
@@ -0,0 +1,16 @@
+create database student_management;
+create table student_management.class ( 
+id int,
+name varchar(255));
+create table student_management.teacher (
+id int,
+name varchar(255) not null,
+age varchar(255) not null,
+country varchar(255));
+create database my_database;
+drop database my_database;
+create table student_management.dataxx (
+id int,
+name varchar(255),
+age varchar(255));
+drop table student_management.dataxx;
