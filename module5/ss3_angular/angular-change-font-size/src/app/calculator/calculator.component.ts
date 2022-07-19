b48144a36abc61ad1a0cfbf73cc3577eb0302f28
C0222G2_LeVanTai <levantai1496@gmail.com>
2022-07-19 11:26:08 +0700

    ss3_angular

---
+++ module5/ss3_angular/angular-change-font-size/src/app/calculator/calculator.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  number1: number;
+  number2: number;
+  result: number;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addition() {
+    return this.result = this.number1 + this.number2;
+  }
+
+  subtraction() {
+    return this.result = this.number1 - this.number2;
+  }
+
+  multiplication() {
+    return this.result = this.number1 * this.number2;
+  }
+  division() {
+    return this.result = this.number1 / this.number2;
+  }
+}
